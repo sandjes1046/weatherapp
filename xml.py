4a5531f9fa1b8c046deb7e53fad1ea5e828d6f9e
sandjes1046 <40582497+sandjes1046@users.noreply.github.com>
2019-04-11 17:11:33 -0500

    Update xml.py
    Can print all the values of all the categories but I cannot print out a specific categories value's

--- xml.py
+++ xml.py
@@ -4,4 +4,6 @@ import requests
 xml_response=requests.get("https://forecast.weather.gov/MapClick.php?lat=25.9879&lon=-97.5555&FcstType=digitalDWML")
 
 root = ET.fromstring(xml_response.content)
-root[1][1].text
+
+for child in root.iter('value'):
+    print(child.tag,child.text)
